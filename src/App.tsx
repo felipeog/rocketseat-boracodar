22d4c911744a2b7900dcf07b13cac8c779d4769c
felipeog <17603069+felipeog@users.noreply.github.com>
2026-01-13 16:42:03 -0300

    step 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import challengesData from './data/challenges.json'
 
 function App() {
   return (
     <div className="app">
       <h1>Bora Codar - Challenge Tracker</h1>
-      <p>React project bootstrapped successfully!</p>
+      <p>Loaded {challengesData.length} challenges</p>
+      <ul className="challenge-list">
+        {challengesData.map((challenge) => (
+          <li key={challenge.id} className="challenge-item">
+            <span className="challenge-id">#{challenge.id}</span>
+            <a href={challenge.url} target="_blank" rel="noopener noreferrer">
+              {challenge.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
